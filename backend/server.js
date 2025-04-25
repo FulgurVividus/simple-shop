446cd8de84ea04f49357757424aa3cb72bf8c0df
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2025-04-25 18:11:55 +0500

    feat: MongoDB setup

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,8 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
 
 const app = express();
 
@@ -7,5 +11,6 @@ app.get("/products", (req, res) => {
 });
 
 app.listen(9000, () => {
+  connectDB();
   console.log(`Server started at http://localhost:9000`);
 });
