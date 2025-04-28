d4719bdb4c89d66ec27df2ac475ef7bbd43b803b
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2025-04-28 22:13:34 +0500

    feat: pre for prod

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import { connectDB } from "./config/db.js";
 import productRouter from "./routes/product.route.js";
 
@@ -8,11 +9,21 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 9000;
 
+const __dirname = path.resolve();
+
 // Allows to accept JSON data in the request body
 app.use(express.json());
 
 // routes
 app.use("/api/products", productRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
